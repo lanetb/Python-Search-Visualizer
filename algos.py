03f2b159afb2384a2b1e89b99ce447fa79f86181
lanetb <lanetb@dukes.jmu.edu>
2023-09-14 20:21:33 -0400

    added astar scafolding

--- algos.py
+++ algos.py
@@ -4,3 +4,9 @@ def dijk (grid, current_box, queue):
             neighbour.queued = True
             neighbour.prior = current_box
             queue.append(neighbour)
+
+def manhatten_heuristic(box, target_box):
+    return abs(box.x - target_box.x) + abs(box.y - target_box.y)
+
+def a_star(grid, current_box, queue, target_box, heristic):
+    pass
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import pygame, sys, random, math #pygame and module systems
+import pygame
+import sys
 from collections import deque
-from tkinter import messagebox, Tk #allows for small dialog boxes to be created
-from algos import dijk
+from tkinter import messagebox, Tk
+from algos import dijk, a_star
+import queue
 
 # Initialize pygame
-size = (width, height) = (850, 480) #width and height of the screen
-pygame.init() #initializes pygame
+size = (width, height) = (850, (480+50))
+pygame.init()
 
 # Create screen
 win = pygame.display.set_mode(size)
@@ -13,14 +15,47 @@ pygame.display.set_caption("Pathfinding")
 clock = pygame.time.Clock()
 
 cols, rows = 85, 48
-
 w = width // cols
-h = height // rows
+h = (height - 50) // rows  # Subtract 50 for the GUI bar
 
+algorithm_type = "Dijkstra"
 grid = []
-queue = deque()
+match algorithm_type:
+    case "Dijkstra":
+        que = deque()
+    case "A*":
+        que = queue.PriorityQueue()
 path = []
 
+# Define GUI variables
+gui_height = 50
+gui_color = (255, 255, 255)
+button_color = (0, 100, 200)
+button_hover_color = (0, 150, 255)
+button_font = pygame.font.Font(None, 36)
+
+# Create a Button class for the GUI
+class Button:
+    def __init__(self, text, x, y, width, height, command):
+        self.text = text
+        self.rect = pygame.Rect(x, y, width, height)
+        self.command = command
+        self.hovered = False
+
+    def draw(self):
+        color = button_hover_color if self.hovered else button_color
+        pygame.draw.rect(win, color, self.rect)
+        text = button_font.render(self.text, True, (255, 255, 255))
+        text_rect = text.get_rect(center=self.rect.center)
+        win.blit(text, text_rect)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            self.hovered = self.rect.collidepoint(event.pos)
+        elif event.type == pygame.MOUSEBUTTONDOWN and self.hovered:
+            self.command()
+
+# Creates the grid
 class Box:
     def __init__(self, x, y):
         self.x, self.y = x, y
@@ -34,7 +69,7 @@ class Box:
         self.prior = None
 
     def draw(self, win, color):
-        pygame.draw.rect(win, color, (self.x * w, self.y * h, w-2, h-2))
+        pygame.draw.rect(win, color, (self.x * w, self.y * h + gui_height, w-2, h-2))
 
     def set_neighbors(self):
         if self.x > 0:
@@ -46,61 +81,54 @@ class Box:
         if self.y < rows - 1:
             self.neighbors.append(grid[self.x][self.y + 1])
 
-#creates the grid
+# ... (rest of your Box class)
+
 for col in range(cols):
-    arr=[]
+    arr = []
     for row in range(rows):
         arr.append(Box(col, row))
     grid.append(arr)
 
-#Sets the neighbors for each box
 for col in range(cols):
     for row in range(rows):
         grid[col][row].set_neighbors()
 
-#creates the start box
 start_box = grid[0][0]
 start_box.start = True
 start_box.visited = True
-queue.append(start_box)
+que.append(start_box)
 
+# Main loop
 def main():
     begin_search = False
     target_box_set = False
     searching = True
     target_box = None
-     #main method houses the main loop
+
     while True:
         for event in pygame.event.get():
-            #quit game
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            #mouse interaction
-            elif event.type == pygame.MOUSEMOTION: #checks for the event type of motion
+            elif event.type == pygame.MOUSEMOTION:
                 x, y = pygame.mouse.get_pos()
-                print(x) #records the position of the mouse
-                #drawing the wall
-                if event.buttons[0] and x < width and x > 0 and y < height and y > 0: #checks if the left mouse button is pressed
-                    i = x // w #records the x position of the mouse
-                    j = y // h #records the y position of the mouse
-                    grid[i][j].wall = True #sets the wall to true
-
-                if event.buttons[2] and not target_box_set and x < width and x > 0 and y < height and y > 0: #checks if the right mouse button is pressed and the target box is not set
-                    i = x // w #records the x position of the mouse
-                    j = y // h #records the y position of the mouse
-                    target_box = grid[i][j] #sets the target box to the current box
-                    target_box.target = True #sets the target box to true
-                    target_box_set = True #sets the target box flag to true
-
-            #keyboard interaction
-            if event.type == pygame.KEYDOWN and target_box_set:
+                if event.buttons[0] and x < width and x > 0 and y < height and y > gui_height:
+                    i = x // w
+                    j = (y - gui_height) // h
+                    grid[i][j].wall = True
+                if event.buttons[2] and not target_box_set and x < width and x > 0 and y < height and y > gui_height:
+                    i = x // w
+                    j = (y - gui_height) // h
+                    target_box = grid[i][j]
+                    target_box.target = True
+                    target_box_set = True
+            elif event.type == pygame.KEYDOWN and target_box_set:
                 if event.key == pygame.K_SPACE:
                     begin_search = True
 
         if begin_search:
-            if len(queue) > 0 and searching:
-                current_box = queue.popleft()
+            if len(que) > 0 and searching:
+                current_box = que.popleft()
                 current_box.visited = True
                 if current_box == target_box:
                     searching = False
@@ -108,14 +136,18 @@ def main():
                         path.append(current_box.prior)
                         current_box = current_box.prior
                 else:
-                    dijk(grid, current_box, queue)
+                    match algorithm_type:
+                        case "Dijkstra": 
+                            dijk(grid, current_box, que)
+                        case "A*":
+                            a_star(grid, current_box, que, target_box, heristic="manhatten")
             else:
                 if searching:
                     Tk().wm_withdraw()
                     messagebox.showinfo("Path Not Found", "There was no path to the target box")
                     searching = False
 
-        win.fill((0, 0, 0)) #window fills color black
+        win.fill((0, 0, 0))
 
         for col in range(cols):
             for row in range(rows):
@@ -124,7 +156,7 @@ def main():
                 if box.queued:
                     box.draw(win, (200, 0, 0))
                 if box.visited:
-                    box.draw(win, (0, 200, 0))
+                    box.draw(win, (0, 255, 0))
                 if box in path:
                     box.draw(win, (0, 0, 200))
                 if box.start:
@@ -132,9 +164,12 @@ def main():
                 if box.target:
                     box.draw(win, (200, 200, 0))
                 if box.wall:
-                    box.draw(win, (190, 190, 190))
+                    box.draw(win, (150, 150, 150))
+
+        # Draw GUI
+        pygame.draw.rect(win, gui_color, (0, 0, width, gui_height-5))
 
 
-        pygame.display.flip() #updates the screen can also use pygame.display.update()
+        pygame.display.flip()
 
 main()
